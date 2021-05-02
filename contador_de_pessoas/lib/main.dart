e9820611fcc245f05a3732bb5810b9d305d67a71
João Pedro <jpedrodaibello1999@outlook.com>
2021-05-02 13:45:15 -0300

    [CDP] Coluna e Textos

--- contador_de_pessoas/lib/main.dart
+++ contador_de_pessoas/lib/main.dart
@@ -3,6 +3,19 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     title: "Contador de pessoas",
-    home: Container(color: Colors.white,),
+    home: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          "Pessoas: 0",
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        Text(
+          "Pode Entrar!",
+          style: TextStyle(
+              color: Colors.white, fontStyle: FontStyle.italic, fontSize: 30),
+        ),
+      ],
+    ),
   ));
 }
