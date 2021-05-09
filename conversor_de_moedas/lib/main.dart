5c0b6d43500f2cc7297259002b6892704ac59fe6
João Pedro <jpedrodaibello1999@outlook.com>
2021-05-09 13:07:38 -0300

    [CDM] Trabalhando com Future

--- conversor_de_moedas/lib/main.dart
+++ conversor_de_moedas/lib/main.dart
@@ -6,10 +6,34 @@ import 'package:http/http.dart' as http;
 var request = Uri.parse("https://api.hgbrasil.com/finance?key=ec8ff24f");
 
 void main() async {
-  http.Response response = await http.get(request);
-  print(json.decode(response.body)["results"]["currencies"]["USD"]);
-
   runApp(MaterialApp(
-    home: Container(),
+    home: Home(),
   ));
+}
+
+Future<Map> getData() async {
+  http.Response response = await http.get(request);
+  return json.decode(response.body);
+}
+
+class Home extends StatefulWidget {
+  const Home({Key key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text("\$ Conversor de Moedas \$"),
+        backgroundColor: Colors.amber,
+        centerTitle: true,
+      ),
+      body: Container(),
+    );
+  }
 }
