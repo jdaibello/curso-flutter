bf46461c210a3936a3a28a917eb1d7bae33d6f87
João Pedro <jpedrodaibello1999@outlook.com>
2021-05-20 13:50:26 -0300

    [ADC] Cartões dos Contatos

--- agenda_de_contatos/lib/ui/home_page.dart
+++ agenda_de_contatos/lib/ui/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:agenda_de_contatos/helpers/contact_helper.dart';
 import 'package:flutter/material.dart';
 
@@ -41,7 +43,56 @@ class _HomePageState extends State<HomePage> {
       body: ListView.builder(
         padding: EdgeInsets.all(10),
         itemCount: contacts.length,
-        itemBuilder: (context, index) {},
+        itemBuilder: (context, index) {
+          return _contactCard(context, index);
+        },
+      ),
+    );
+  }
+
+  Widget _contactCard(BuildContext context, int index) {
+    return GestureDetector(
+      child: Card(
+        child: Padding(
+          padding: EdgeInsets.all(10),
+          child: Row(
+            children: [
+              Container(
+                width: 80,
+                height: 80,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: contacts[index].img != null
+                        ? FileImage(File(contacts[index].img))
+                        : AssetImage("images/person.png"),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      contacts[index].name ?? "",
+                      style:
+                          TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      contacts[index].email ?? "",
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    Text(
+                      contacts[index].phone ?? "",
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
