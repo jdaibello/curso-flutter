d3e7f1f4444069b176b09f3cacfb7f4d62f963f1
João Pedro <jpedrodaibello1999@outlook.com>
2021-05-19 21:07:26 -0300

    [BDG] Iniciando o Layout

--- buscador_de_gifs/lib/main.dart
+++ buscador_de_gifs/lib/main.dart
@@ -4,5 +4,15 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: HomePage(),
+    theme: ThemeData(
+        hintColor: Colors.white,
+        primaryColor: Colors.white,
+        inputDecorationTheme: InputDecorationTheme(
+          enabledBorder:
+              OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+          focusedBorder:
+              OutlineInputBorder(borderSide: BorderSide(color: Colors.blue)),
+          hintStyle: TextStyle(color: Colors.blue),
+        )),
   ));
 }
