075f971f464b2c6daed848a647064195bcc9fd89
João Pedro <jpedrodaibello1999@outlook.com>
2021-05-21 11:21:21 -0300

    [CHT] Configurando o Projeto

---
+++ chat/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+
+  Firestore.instance
+      .collection("col")
+      .document("doc")
+      .setData({"texto": "daniel"});
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Container(),
+    );
+  }
+}
