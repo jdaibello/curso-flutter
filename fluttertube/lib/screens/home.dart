d8dd6a09842b7224947719af16656e6f57825d81
João Pedro <jpedrodaibello1999@outlook.com>
2021-05-23 12:46:47 -0300

    [FTB] Criando o Layout da Tela Principal

--- fluttertube/lib/screens/home.dart
+++ fluttertube/lib/screens/home.dart
@@ -5,6 +5,30 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Container(
+          height: 25,
+          child: Image.asset("images/yt_logo_rgb_dark.png"),
+        ),
+        elevation: 0,
+        backgroundColor: Colors.black87,
+        actions: [
+          Align(
+            alignment: Alignment.center,
+            child: Text("0"),
+          ),
+          IconButton(
+            icon: Icon(Icons.star),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Container(),
+    );
   }
 }
