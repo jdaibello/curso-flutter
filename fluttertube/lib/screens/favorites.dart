4a00c055f532be01127e2dc3a58118a1452ca624
João Pedro <jpedrodaibello1999@outlook.com>
2021-05-24 12:37:15 -0300

    [FTB] Abrindo o Player de Vídeo

--- fluttertube/lib/screens/favorites.dart
+++ fluttertube/lib/screens/favorites.dart
@@ -1,5 +1,7 @@
 import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_youtube/flutter_youtube.dart';
+import 'package:fluttertube/api.dart';
 import 'package:fluttertube/blocs/favorite_bloc.dart';
 import 'package:fluttertube/models/video.dart';
 
@@ -24,7 +26,10 @@ class Favorites extends StatelessWidget {
           return ListView(
             children: snapshot.data.values.map((v) {
               return InkWell(
-                onTap: () {},
+                onTap: () {
+                  FlutterYoutube.playYoutubeVideoById(
+                      apiKey: API_KEY, videoId: v.id);
+                },
                 onLongPress: () {
                   bloc.toggleFavorite(v);
                 },
