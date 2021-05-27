bff9527a4fc05f3e22ddd3e8da8305fc6d4a3794
João Pedro <jpedrodaibello1999@outlook.com>
2021-05-27 10:19:38 -0300

    [XLO] Mudando de Página pelo Drawer

--- xlo_mobx/lib/components/custom_drawer/page_section.dart
+++ xlo_mobx/lib/components/custom_drawer/page_section.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 import 'package:xlo_mobx/components/custom_drawer/page_tile.dart';
+import 'package:xlo_mobx/stores/page_store.dart';
 
 class PageSection extends StatelessWidget {
-  const PageSection({Key key}) : super(key: key);
+  PageSection({Key key}) : super(key: key);
+
+  final PageStore pageStore = GetIt.I<PageStore>();
 
   @override
   Widget build(BuildContext context) {
@@ -11,32 +15,42 @@ class PageSection extends StatelessWidget {
         PageTile(
           label: 'Anúncios',
           iconData: Icons.list,
-          onTap: () {},
-          highlighted: true,
+          onTap: () {
+            pageStore.setPage(0);
+          },
+          highlighted: pageStore.page == 0,
         ),
         PageTile(
           label: 'Inserir Anúncio',
           iconData: Icons.edit,
-          onTap: () {},
-          highlighted: false,
+          onTap: () {
+            pageStore.setPage(1);
+          },
+          highlighted: pageStore.page == 1,
         ),
         PageTile(
           label: 'Chat',
           iconData: Icons.chat,
-          onTap: () {},
-          highlighted: false,
+          onTap: () {
+            pageStore.setPage(2);
+          },
+          highlighted: pageStore.page == 2,
         ),
         PageTile(
           label: 'Favoritos',
           iconData: Icons.favorite,
-          onTap: () {},
-          highlighted: false,
+          onTap: () {
+            pageStore.setPage(3);
+          },
+          highlighted: pageStore.page == 3,
         ),
         PageTile(
           label: 'Minha Conta',
           iconData: Icons.person,
-          onTap: () {},
-          highlighted: false,
+          onTap: () {
+            pageStore.setPage(4);
+          },
+          highlighted: pageStore.page == 4,
         ),
       ],
     );
--- xlo_mobx/lib/main.dart
+++ xlo_mobx/lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart' as DotEnv;
+import 'package:get_it/get_it.dart';
 import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
 import 'package:xlo_mobx/screens/base/base_screen.dart';
+import 'package:xlo_mobx/stores/page_store.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await DotEnv.load(fileName: ".env");
   await initializeParse();
+  setupLocators();
   runApp(MyApp());
 }
 
@@ -18,6 +21,10 @@ Future<void> initializeParse() async {
     autoSendSessionId: true,
     debug: true,
   );
+}
+
+void setupLocators() {
+  GetIt.I.registerSingleton(PageStore());
 }
 
 class MyApp extends StatelessWidget {
