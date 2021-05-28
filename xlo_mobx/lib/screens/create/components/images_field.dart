df71bd5b55052f0bcfd70aa7605e045fbfc34d36
João Pedro <jpedrodaibello1999@outlook.com>
2021-05-28 12:10:13 -0300

    [XLO] Começando o Campo de Imagens

---
+++ xlo_mobx/lib/screens/create/components/images_field.dart
@@ -0,0 +1,54 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ImagesField extends StatelessWidget {
+  const ImagesField({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.grey[200],
+      height: 120,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: 5,
+        itemBuilder: (_, index) {
+          return Padding(
+            padding: const EdgeInsets.fromLTRB(8, 8, 0, 8),
+            child: GestureDetector(
+              onTap: () {
+                if (Platform.isAndroid) {
+                  showModalBottomSheet(
+                    context: context,
+                    builder: null,
+                  );
+                } else {
+                  showCupertinoModalPopup(
+                    context: context,
+                    builder: null,
+                  );
+                }
+              },
+              child: CircleAvatar(
+                radius: 44,
+                backgroundColor: Colors.grey[300],
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.camera_alt,
+                      size: 40,
+                      color: Colors.white,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
--- xlo_mobx/lib/screens/create/create_screen.dart
+++ xlo_mobx/lib/screens/create/create_screen.dart
@@ -2,6 +2,7 @@ import 'package:brasil_fields/brasil_fields.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:xlo_mobx/components/custom_drawer/custom_drawer.dart';
+import 'package:xlo_mobx/screens/create/components/images_field.dart';
 
 class CreateScreen extends StatelessWidget {
   const CreateScreen({Key key}) : super(key: key);
@@ -23,6 +24,7 @@ class CreateScreen extends StatelessWidget {
         centerTitle: true,
       ),
       body: Card(
+        clipBehavior: Clip.antiAlias,
         margin: const EdgeInsets.symmetric(horizontal: 16),
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(16),
@@ -31,6 +33,7 @@ class CreateScreen extends StatelessWidget {
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
+            ImagesField(),
             TextFormField(
               decoration: InputDecoration(
                 labelText: 'Título *',
