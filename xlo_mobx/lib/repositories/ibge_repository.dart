200f806d33400dc4a30507a32aa567dfe528ebc9
João Pedro <jpedrodaibello1999@outlook.com>
2021-05-31 11:00:49 -0300

    [XLO] Obtendo a Lista de Estados

---
+++ xlo_mobx/lib/repositories/ibge_repository.dart
@@ -0,0 +1,22 @@
+import 'package:dio/dio.dart';
+import 'package:xlo_mobx/models/uf.dart';
+
+class IBGERepository {
+  Future<List<UF>> getUFListFromApi() async {
+    const endpoint =
+        'https://servicodados.ibge.gov.br/api/v1/localidades/estados';
+
+    try {
+      final response = await Dio().get<List>(endpoint);
+
+      final ufList = response.data.map<UF>((j) => UF.fromJson(j)).toList()
+        ..sort((a, b) => a.name.toLowerCase().compareTo(b.name.toLowerCase()));
+
+      return ufList;
+    } on DioError {
+      return Future.error('Falha ao obter lista de Estados');
+    }
+  }
+
+  getCityListFromApi() {}
+}
