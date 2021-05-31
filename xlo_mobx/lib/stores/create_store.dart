09e709f4ff9c0c27a6b23e355dff3d6f824dc7ea
João Pedro <jpedrodaibello1999@outlook.com>
2021-05-31 10:41:39 -0300

    [XLO] Selecionando uma Categoria

--- xlo_mobx/lib/stores/create_store.dart
+++ xlo_mobx/lib/stores/create_store.dart
@@ -1,4 +1,5 @@
 import 'package:mobx/mobx.dart';
+import 'package:xlo_mobx/models/category.dart';
 
 part 'create_store.g.dart';
 
@@ -6,4 +7,10 @@ class CreateStore = _CreateStore with _$CreateStore;
 
 abstract class _CreateStore with Store {
   ObservableList images = ObservableList();
+
+  @observable
+  Category category;
+
+  @action
+  void setCategory(Category value) => category = value;
 }
