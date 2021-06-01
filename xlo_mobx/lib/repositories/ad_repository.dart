7d4022d71f096fd1cc15d273093a9594e030b3c6
João Pedro <jpedrodaibello1999@outlook.com>
2021-06-01 13:49:22 -0300

    [XLO] Salvando o Anúncio no Parse

--- xlo_mobx/lib/repositories/ad_repository.dart
+++ xlo_mobx/lib/repositories/ad_repository.dart
@@ -4,10 +4,41 @@ import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
 import 'package:path/path.dart' as path;
 import 'package:xlo_mobx/models/ad.dart';
 import 'package:xlo_mobx/repositories/parse_errors.dart';
+import 'package:xlo_mobx/repositories/table_keys.dart';
 
 class AdRepository {
   Future<void> save(Ad ad) async {
     final parseImages = await saveImages(ad.images);
+
+    final parseUser = await ParseUser.currentUser();
+
+    final adObject = ParseObject(keyAdTable);
+
+    final parseAcl = ParseACL(owner: parseUser);
+    parseAcl.setPublicReadAccess(allowed: true);
+    parseAcl.setPublicWriteAccess(allowed: false);
+    adObject.setACL(parseAcl);
+
+    adObject.set<String>(keyAdTitle, ad.title);
+    adObject.set<String>(keyAdDescription, ad.description);
+    adObject.set<bool>(keyAdHidePhone, ad.hidePhone);
+    adObject.set<num>(keyAdPrice, ad.price);
+    adObject.set<int>(keyAdStatus, ad.status.index);
+
+    adObject.set<String>(keyAdDistrict, ad.address.district);
+    adObject.set<String>(keyAdCity, ad.address.city.name);
+    adObject.set<String>(keyAdFederativeUnit, ad.address.uf.initials);
+    adObject.set<String>(keyAdPostalCode, ad.address.cep);
+
+    adObject.set<List<ParseFile>>(keyAdImages, parseImages);
+
+    adObject.set<ParseUser>(keyAdOwner, parseUser);
+
+    adObject.set<ParseObject>(keyAdCategory,
+        ParseObject(keyAdTable)..set(keyCategoryId, ad.category.id));
+
+    final response = await adObject.save();
+    print(response.success);
   }
 
   Future<List<ParseFile>> saveImages(List images) async {
