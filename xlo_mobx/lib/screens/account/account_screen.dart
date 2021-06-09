c6f926c442331e13022038b4abc926103e94c557
João Pedro <jpedrodaibello1999@outlook.com>
2021-06-09 10:33:09 -0300

    [XLO] Criando a Tela de Minha Conta

---
+++ xlo_mobx/lib/screens/account/account_screen.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:xlo_mobx/components/custom_drawer/custom_drawer.dart';
+import 'package:xlo_mobx/stores/user_manager_store.dart';
+
+class AccountScreen extends StatelessWidget {
+  const AccountScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Minha Conta'),
+        centerTitle: true,
+      ),
+      drawer: CustomDrawer(),
+      body: Center(
+        child: Card(
+          margin: const EdgeInsets.symmetric(horizontal: 32),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16),
+          ),
+          elevation: 8,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                height: 140,
+                child: Stack(
+                  children: [
+                    Align(
+                      alignment: Alignment.center,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text(
+                            GetIt.I<UserManagerStore>().user.name,
+                            style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.purple,
+                              fontWeight: FontWeight.w900,
+                            ),
+                          ),
+                          Text(
+                            GetIt.I<UserManagerStore>().user.email,
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.grey[700],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.topRight,
+                      child: FlatButton(
+                        child: Text('EDITAR'),
+                        textColor: Colors.purple,
+                        onPressed: () {},
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Divider(),
+              ListTile(
+                title: Text(
+                  'Meus anúncios',
+                  style: TextStyle(
+                    color: Colors.purple,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                trailing: Icon(Icons.keyboard_arrow_right),
+                onTap: () {},
+              ),
+              ListTile(
+                title: Text(
+                  'Favoritos',
+                  style: TextStyle(
+                    color: Colors.purple,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                trailing: Icon(Icons.keyboard_arrow_right),
+                onTap: () {},
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- xlo_mobx/lib/screens/base/base_screen.dart
+++ xlo_mobx/lib/screens/base/base_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:mobx/mobx.dart';
+import 'package:xlo_mobx/screens/account/account_screen.dart';
 import 'package:xlo_mobx/screens/create/create_screen.dart';
 import 'package:xlo_mobx/screens/home/home_screen.dart';
 import 'package:xlo_mobx/stores/page_store.dart';
@@ -38,9 +39,7 @@ class _BaseScreenState extends State<BaseScreen> {
           Container(
             color: Colors.grey,
           ),
-          Container(
-            color: Colors.brown,
-          ),
+          AccountScreen(),
         ],
       ),
     );
