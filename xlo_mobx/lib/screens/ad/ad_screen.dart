7067a545306ff33b43e2c3294a56490c0f7b658c
João Pedro <jpedrodaibello1999@outlook.com>
2021-06-09 09:26:32 -0300

    [XLO] Exibindo a Barra Inferior do Anúncio

--- xlo_mobx/lib/screens/ad/ad_screen.dart
+++ xlo_mobx/lib/screens/ad/ad_screen.dart
@@ -2,6 +2,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:carousel_pro/carousel_pro.dart';
 import 'package:flutter/material.dart';
 import 'package:xlo_mobx/models/ad.dart';
+import 'package:xlo_mobx/screens/ad/components/bottom_bar.dart';
 import 'package:xlo_mobx/screens/ad/components/description_panel.dart';
 import 'package:xlo_mobx/screens/ad/components/location_panel.dart';
 import 'package:xlo_mobx/screens/ad/components/main_panel.dart';
@@ -20,36 +21,41 @@ class AdScreen extends StatelessWidget {
         title: Text('Anúncio'),
         centerTitle: true,
       ),
-      body: ListView(
+      body: Stack(
         children: [
-          Container(
-            height: 280,
-            child: Carousel(
-              images: ad.images
-                  .map((url) => CachedNetworkImageProvider(url))
-                  .toList(),
-              dotSize: 4,
-              dotBgColor: Colors.transparent,
-              dotColor: Colors.orange,
-              autoplay: false,
-            ),
+          ListView(
+            children: [
+              Container(
+                height: 280,
+                child: Carousel(
+                  images: ad.images
+                      .map((url) => CachedNetworkImageProvider(url))
+                      .toList(),
+                  dotSize: 4,
+                  dotBgColor: Colors.transparent,
+                  dotColor: Colors.orange,
+                  autoplay: false,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    MainPanel(ad),
+                    Divider(color: Colors.grey[500]),
+                    DescriptionPanel(ad),
+                    Divider(color: Colors.grey[500]),
+                    LocationPanel(ad),
+                    Divider(color: Colors.grey[500]),
+                    UserPanel(ad),
+                    SizedBox(height: 120),
+                  ],
+                ),
+              ),
+            ],
           ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                MainPanel(ad),
-                Divider(color: Colors.grey[500]),
-                DescriptionPanel(ad),
-                Divider(color: Colors.grey[500]),
-                LocationPanel(ad),
-                Divider(color: Colors.grey[500]),
-                UserPanel(ad),
-                SizedBox(height: 16),
-              ],
-            ),
-          ),
+          BottomBar(ad),
         ],
       ),
     );
