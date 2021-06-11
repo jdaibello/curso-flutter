410efc2df947f6933eb7c445c3a55db06d68ca99
João Pedro <jpedrodaibello1999@outlook.com>
2021-06-11 10:51:41 -0300

    [XLO] Bloqueando as Telas com Login

--- xlo_mobx/lib/components/custom_drawer/page_section.dart
+++ xlo_mobx/lib/components/custom_drawer/page_section.dart
@@ -1,15 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:xlo_mobx/components/custom_drawer/page_tile.dart';
+import 'package:xlo_mobx/screens/login/login_screen.dart';
 import 'package:xlo_mobx/stores/page_store.dart';
+import 'package:xlo_mobx/stores/user_manager_store.dart';
 
 class PageSection extends StatelessWidget {
   PageSection({Key key}) : super(key: key);
 
   final PageStore pageStore = GetIt.I<PageStore>();
+  final UserManagerStore userManagerStore = GetIt.I<UserManagerStore>();
 
   @override
   Widget build(BuildContext context) {
+    Future<void> verifyLoginAndSetPage(int page) async {
+      if (userManagerStore.isLoggedIn) {
+        pageStore.setPage(page);
+      } else {
+        final result = await Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (_) => LoginScreen(),
+          ),
+        );
+        if (result != null && result) {
+          pageStore.setPage(page);
+        }
+      }
+    }
+
     return Column(
       children: [
         PageTile(
@@ -24,7 +42,7 @@ class PageSection extends StatelessWidget {
           label: 'Inserir Anúncio',
           iconData: Icons.edit,
           onTap: () {
-            pageStore.setPage(1);
+            verifyLoginAndSetPage(1);
           },
           highlighted: pageStore.page == 1,
         ),
@@ -32,7 +50,7 @@ class PageSection extends StatelessWidget {
           label: 'Chat',
           iconData: Icons.chat,
           onTap: () {
-            pageStore.setPage(2);
+            verifyLoginAndSetPage(2);
           },
           highlighted: pageStore.page == 2,
         ),
@@ -40,7 +58,7 @@ class PageSection extends StatelessWidget {
           label: 'Favoritos',
           iconData: Icons.favorite,
           onTap: () {
-            pageStore.setPage(3);
+            verifyLoginAndSetPage(3);
           },
           highlighted: pageStore.page == 3,
         ),
@@ -48,7 +66,7 @@ class PageSection extends StatelessWidget {
           label: 'Minha Conta',
           iconData: Icons.person,
           onTap: () {
-            pageStore.setPage(4);
+            verifyLoginAndSetPage(4);
           },
           highlighted: pageStore.page == 4,
         ),
