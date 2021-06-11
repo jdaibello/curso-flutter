4e1b5732946c61273dc5621e5ddb69a046534ac6
João Pedro <jpedrodaibello1999@outlook.com>
2021-06-11 13:25:47 -0300

    [XLO] Carregando os Favoritos do Usuário

--- xlo_mobx/lib/repositories/favorite_repository.dart
+++ xlo_mobx/lib/repositories/favorite_repository.dart
@@ -5,6 +5,26 @@ import 'package:xlo_mobx/repositories/parse_errors.dart';
 import 'package:xlo_mobx/repositories/table_keys.dart';
 
 class FavoriteRepository {
+  Future<List<Ad>> getFavorites(User user) async {
+    final queryBuilder =
+        QueryBuilder<ParseObject>(ParseObject(keyFavoritesTable));
+
+    queryBuilder.whereEqualTo(keyFavoritesOwner, user.id);
+    queryBuilder.includeObject([keyFavoritesAd, 'ad.owner']);
+
+    final response = await queryBuilder.query();
+
+    if (response.success && response.results != null) {
+      return response.results
+          .map((po) => Ad.fromParse(po.get(keyFavoritesAd)))
+          .toList();
+    } else if (response.success && response.results == null) {
+      return [];
+    } else {
+      return Future.error(ParseErrors.getDescription(response.error.code));
+    }
+  }
+
   Future<void> save({Ad ad, User user}) async {
     final favoriteObject = ParseObject(keyFavoritesTable);
 
