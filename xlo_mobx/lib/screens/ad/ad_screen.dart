f90d186da4934d7d0f1237f5e20b8102d05ad858
João Pedro <jpedrodaibello1999@outlook.com>
2021-06-11 12:01:15 -0300

    [XLO] Desfavoritando um Anúncio

--- xlo_mobx/lib/screens/ad/ad_screen.dart
+++ xlo_mobx/lib/screens/ad/ad_screen.dart
@@ -1,6 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:carousel_pro/carousel_pro.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:get_it/get_it.dart';
 import 'package:xlo_mobx/models/ad.dart';
 import 'package:xlo_mobx/screens/ad/components/bottom_bar.dart';
@@ -28,10 +29,16 @@ class AdScreen extends StatelessWidget {
         centerTitle: true,
         actions: [
           if (ad.status == AdStatus.ACTIVE && userManagerStore.isLoggedIn)
-            IconButton(
-              icon: Icon(Icons.favorite_border),
-              onPressed: () => favoriteStore.toggleFavorite(ad),
-            ),
+            Observer(builder: (_) {
+              return IconButton(
+                icon: Icon(
+                  favoriteStore.favoriteList.any((a) => a.id == ad.id)
+                      ? Icons.favorite
+                      : Icons.favorite_border,
+                ),
+                onPressed: () => favoriteStore.toggleFavorite(ad),
+              );
+            }),
         ],
       ),
       body: Stack(
