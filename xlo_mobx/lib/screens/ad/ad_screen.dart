6dea8e85d2109929a7b345fcced3f4829305bb41
João Pedro <jpedrodaibello1999@outlook.com>
2021-06-11 11:46:45 -0300

    [XLO] Favoritando um Anúncio

--- xlo_mobx/lib/screens/ad/ad_screen.dart
+++ xlo_mobx/lib/screens/ad/ad_screen.dart
@@ -1,17 +1,23 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:carousel_pro/carousel_pro.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 import 'package:xlo_mobx/models/ad.dart';
 import 'package:xlo_mobx/screens/ad/components/bottom_bar.dart';
 import 'package:xlo_mobx/screens/ad/components/description_panel.dart';
 import 'package:xlo_mobx/screens/ad/components/location_panel.dart';
 import 'package:xlo_mobx/screens/ad/components/main_panel.dart';
 import 'package:xlo_mobx/screens/ad/components/user_panel.dart';
+import 'package:xlo_mobx/stores/favorite_store.dart';
+import 'package:xlo_mobx/stores/user_manager_store.dart';
 
 class AdScreen extends StatelessWidget {
-  const AdScreen(this.ad, {Key key}) : super(key: key);
+  AdScreen(this.ad, {Key key}) : super(key: key);
 
   final Ad ad;
+
+  final UserManagerStore userManagerStore = GetIt.I<UserManagerStore>();
+  final FavoriteStore favoriteStore = GetIt.I<FavoriteStore>();
 
   @override
   Widget build(BuildContext context) {
@@ -20,6 +26,13 @@ class AdScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Anúncio'),
         centerTitle: true,
+        actions: [
+          if (ad.status == AdStatus.ACTIVE && userManagerStore.isLoggedIn)
+            IconButton(
+              icon: Icon(Icons.favorite_border),
+              onPressed: () => favoriteStore.toggleFavorite(ad),
+            ),
+        ],
       ),
       body: Stack(
         children: [
